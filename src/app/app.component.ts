b362fa30e70f2c0fb3fe127d05152b426cd65415
jonathan <jonathan_mantilla@hotmail.com>
2019-09-13 12:23:16 -0500

    Primera version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+export interface Food{
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ctrang';
+
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'España'},
+    {value: 'pizza-1', viewValue: 'Francia'},
+    {value: 'tacos-2', viewValue: 'Belgica'}
+  ];
+
+  color = 'primary';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule, MatInputModule, MatCheckboxModule, MatNativeDateModule, MatFormFieldModule, MatSelectModule, MatRadioModule, MatSliderModule, MatSlideToggleModule} from '@angular/material';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatSliderModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
